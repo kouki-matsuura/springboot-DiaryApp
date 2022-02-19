3329a12fe4298cfc28fd9350bae9d2243b7fa854
松浦　昂生 <kouki19991222@icloud.com>
2022-02-19 09:46:36 +0900

    [add]バリデーションの追加

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -5,13 +5,22 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.GroupOrder;
+import com.example.demo.form.ProfileForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/profile")
+@Slf4j
 public class ProfileController {
 	
 	@Autowired
@@ -24,11 +33,24 @@ public class ProfileController {
 	}
 	
 	@GetMapping("/edit")
-	public String getEdit(Model model) {
+	public String getEdit(Model model, @ModelAttribute ProfileForm form) {
 		//性別を取得
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
 		model.addAttribute("genderMap", genderMap);
 		
 		return "profile/edit";
+	}
+	
+	@PostMapping("/edit")
+	public String postEdit(Model model, @ModelAttribute @Validated(GroupOrder.class) ProfileForm form, BindingResult bindingResult) {
+		{
+		//入力チェック
+		if (bindingResult.hasErrors()) {
+			//プロフィール編集画面に戻る
+			return getEdit(model, form);
+		}
+		log.info(form.toString());
+		//プロフィール画面にリダイレクト
+		return "redirect:/profile/profile";
 	}
 }
--- src/main/java/com/example/demo/form/ProfileForm.java
+++ src/main/java/com/example/demo/form/ProfileForm.java
@@ -1,8 +1,29 @@
 package com.example.demo.form;
 
+import java.util.Date;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class ProfileForm {
+	@NotBlank(groups=ValidGroup1.class)
+	private String name;
 	
+	@Min(value=18, groups=ValidGroup2.class)
+	@Max(value=100, groups=ValidGroup2.class)
+	private Integer age;
+	
+	@DateTimeFormat(pattern="yyyy/MM/dd")
+	@NotNull(groups=ValidGroup1.class)
+	private Date birthday;
+	
+	@NotNull(groups=ValidGroup1.class)
+	private Integer gender;
 }
